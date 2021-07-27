304cc2c684b6f3468de061758e29dbd0233ad640
syafiq <abdillah.syafiq@gmail.com>
2021-07-27 07:18:31 +0700

    Initial commit

---
+++ lib/screens/home/components/list_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ListItem extends StatelessWidget {
+  const ListItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> entries = <String>['A', 'B', 'C'];
+    final List<int> colorCodes = <int>[600, 500, 100];
+
+    return ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: entries.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 100,
+            color: Colors.amber[colorCodes[index]],
+            child: Center(child: Text('Entry ${entries[index]}')),
+            margin: const EdgeInsets.only(bottom: 8),
+          );
+        });
+  }
+}
---
+++ lib/screens/home/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'components/list_view.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Learn Flutter"),
+      ),
+      body: ListItem(),
+    );
+  }
+}
